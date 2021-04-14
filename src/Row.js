b90a5f9fff07562c9d51869bc62c718d5cd80b7e
aryan1222 <chhabraaryan12@gmail.com>
2021-04-14 11:48:42 +0530

    get all movies from TMDB

---
+++ src/Row.js
@@ -0,0 +1,41 @@
+import React, {useState, useEffect} from 'react'
+import axios from './axios';
+const baseUrl = "https://image.tmdb.org/t/p/original/";
+
+function Row({title, fetchUrl}) {
+
+    const [movies, setMovies] = useState([]);
+
+    useEffect(()=>{
+        async function fetchData(){
+            const request = await axios.get(fetchUrl); 
+            setMovies(request.data.results);
+            return request;
+        }
+
+        fetchData();
+    }, [fetchUrl])
+
+    console.table(movies);
+
+    const list = movies.map((movie)=>
+        <img 
+            className="row_poster" 
+            src={`${baseUrl}${movie.poster_path}`}  
+            alt={movie.name} 
+            style={{width:'100px'}}
+            />
+        )
+
+    return (
+        <div className="row">
+            <h1>{title}</h1>
+
+            <div className="row_posters">
+                {list}
+            </div>
+        </div>
+    )
+}
+
+export default Row
